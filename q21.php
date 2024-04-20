c021c52777eae36c6415935d02c61084f0a36c15
Omar Dachia <omardachia@gmail.com>
2024-04-20 15:32:18 +0100

    sdfgdf fdd fdf

---
+++ q21.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question 2</title>
+    <style>
+        #main{
+            margin:10%;
+            background:#f7ffe6;
+            
+        }
+        #header{
+            background:#88cc00;
+            text-align:center;
+        }
+        #input{
+          padding-left:10%;
+          padding-right:10%;
+          padding-top:1%;
+        }
+    </style>
+</head>
+<body>
+    <div id="main">
+        <div id="header">
+          <h1>Question</h1>
+          <p>
+          19.	WAP to enter the %age of a student & print whether he is PASS or FAIL.
+            </p>
+        </div>
+        <div id="input">
+        <form action="" method="POST">
+           <p>
+                <label for="">Score:</label>
+                <input type="number" name="score" placeholder="Number">
+           </p>
+           
+           <input type="submit" value="Compute" name="submit"/>
+        </form>
+        </div>
+        <div id="result">
+          <?php 
+            //  print_r($_POST);
+            $score= $_POST['score'];
+            
+            if($score > 45)
+            {
+                echo "Congratulation, you have Pass this subject with this ".$score." Score";
+            }
+            else
+            {
+                echo "Sorry, unffurtunately you failed this subject with this ".$score." Score";
+            }
+           
+          ?>
+        </div>
+    </div>
+
+</body>
+</html>
