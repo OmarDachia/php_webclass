f10b4a6e1fe2850fbf07d9cd268b80d45961eee2
Omar Dachia <omardachia@gmail.com>
2024-04-21 12:35:12 +0100

    Q22 content

---
+++ q22.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question 2</title>
+    <style>
+        #main{
+            margin:10%;
+            background:#f7ffe6;
+            
+        }
+        #header{
+            background:#88cc00;
+            text-align:center;
+        }
+        #input{
+          padding-left:10%;
+          padding-right:10%;
+          padding-top:1%;
+        }
+    </style>
+</head>
+<body>
+    <div id="main">
+        <div id="header">
+          <h1>Question</h1>
+          <p>
+          21.	Write a program to accept a grade and declare the equivalent description
+            </p>
+        </div>
+        <div id="input">
+        <form action="" method="POST">
+           <p>
+                <label for="">Grade:</label>
+                <input type="text" name="grade" placeholder="Enter a grade">
+           </p>
+           
+           <input type="submit" value="Compute" name="submit"/>
+        </form>
+        </div>
+        <div id="result">
+          <?php 
+            //  print_r($_POST);
+            $grade= $_POST['grade'];
+            
+            if($grade == 'A')
+            {
+                echo "Excellent";
+            }
+            else if($grade == 'B')
+            {
+                echo "Very Good";
+            }
+            else if($grade == 'C')
+            {
+                echo "Good";
+            }
+            else if($grade == 'D')
+            {
+                echo "Poor";
+            }
+            else if($grade == 'E')
+            {
+                echo "Fair";
+            }
+            else  if($grade == 'F')
+            {
+                echo "Fail";
+            }
+            else
+            {
+                echo "Sorry, unfutunately you have invalid input";
+            }
+           
+          ?>
+        </div>
+    </div>
+
+</body>
+</html>
