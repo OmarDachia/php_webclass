2a3a32270f9ba14532c399c3707d5677ec351dc2
Omar Dachia <omardachia@gmail.com>
2024-05-05 11:16:54 +0100

    Q31 logic implemented

--- q31.php
+++ q31.php
@@ -26,14 +26,18 @@
         <div id="header">
           <h1>Question</h1>
           <p>
-          30.	WAP to display arithmetic operations using switch-case statement.
+          31.	WAP to print a name a message 10 times.
           </p>
         </div>
         <div id="input">
         <form action="" method="POST">
            <p>
-                <label for="">operations:</label>
-                <input type="text" name="operator" placeholder="Enter a operator">
+                <label for="">Name:</label>
+                <input type="text" name="name" placeholder="Enter a Name">
+           </p>
+           <p>
+                <label for="">Massage:</label>
+                <input type="text" name="msg" placeholder="Enter a Massage">
            </p>
            
            <input type="submit" value="Compute" name="submit"/>
@@ -42,23 +46,20 @@
         <div id="result">
           <?php 
             //  print_r($_POST);
-            $operator= $_POST['operator'];
-            switch($operator){
-                case "+":
-                    echo "Addition";
-                    break;
-                case "-":
-                    echo "Subtraction";
-                    break;
-                case "*":
-                    echo "Multipication";
-                    break;
-                case "/":
-                    echo "Division";
-                    break;
-                case "%":
-                    echo "Modulo";
-                    break;
+            $name= $_POST['name'];
+            $msg= $_POST['msg'];
+            
+            echo "using for loop";
+            for($i=0;$i>0;$i++)
+            {
+                echo $name. " " . $msg;
+            }
+
+            echo "using while";
+
+            $j=0;
+            while($j>0){
+                echo $name. " " . $msg;
             }
            
           ?>
